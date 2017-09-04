e1f7d319c2efd8278b9cab9afbeda64e38b6370b
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-03 19:25:49 -0700

    Add styling, components, containers, reducers, wire to actions, set up weather API, use redux-promise, google maps, react sparklines and axios.

---
+++ weatherview/src/components/google_map.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class GoogleMap extends React.Component {
+    componentDidMount() {
+        new google.maps.Map(this.refs.map, {
+            zoom: 12,
+            center: {
+                lat: this.props.lat,
+                lng: this.props.lon
+            }
+        });
+    }
+    render() {
+        return <div ref="map" />;
+    }
+}
+
+export default GoogleMap;
+
+// import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+
+// export default props => {
+//     return (
+//         <GoogleMapLoader
+//             containerElement={ <div style={{height: '100%'}} /> }
+//             googleMapElement={
+//                 <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
+//             }
+//         />
+//     );
+// }
+
